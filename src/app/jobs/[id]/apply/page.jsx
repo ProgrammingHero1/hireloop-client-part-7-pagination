f44db2507140fc808db78adbbfcc304b25f525a1
ProgrammingHero1 <programming.hero1@gmail.com>
2026-06-05 13:31:25 -0500

    redirect to login page

--- src/app/jobs/[id]/apply/page.jsx
+++ src/app/jobs/[id]/apply/page.jsx
@@ -1,6 +1,16 @@
+import { getUserSession } from '@/lib/core/session';
+import { redirect } from 'next/navigation';
 import React from 'react';
 
-const ApplyPage = () => {
+const ApplyPage = async ({ params }) => {
+    const { id } = await params;
+
+    const user = await getUserSession();
+
+    if(!user) {
+        redirect(`/auth/signin?redirect=/jobs/${id}/apply`);
+    }
+
     return (
         <div>
             <h2>Apply for this Job</h2>
