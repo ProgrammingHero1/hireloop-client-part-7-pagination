807251b0d9c5844730e4b57bf7dbbf85913afbc9
ProgrammingHero1 <programming.hero1@gmail.com>
2026-06-05 13:59:02 -0500

    redirect after login and allow only job seeker to apply

--- src/app/jobs/[id]/apply/page.jsx
+++ src/app/jobs/[id]/apply/page.jsx
@@ -1,6 +1,8 @@
+import { getJobById } from '@/lib/api/jobs';
 import { getUserSession } from '@/lib/core/session';
 import { redirect } from 'next/navigation';
 import React from 'react';
+import JobApply from './JobApply';
 
 const ApplyPage = async ({ params }) => {
     const { id } = await params;
@@ -11,9 +13,20 @@ const ApplyPage = async ({ params }) => {
         redirect(`/auth/signin?redirect=/jobs/${id}/apply`);
     }
 
+    if(user.role !== 'seeker') {
+        return (
+            <div className="w-full min-h-screen bg-zinc-950 flex flex-col justify-center items-center text-white p-6">
+                <p className="text-zinc-400 text-lg">Only job seekers can apply for positions. Please sign in with a seeker account to proceed.</p>
+            </div>
+        );
+    }
+
+    const job = await getJobById(id);
+
+
     return (
         <div>
-            <h2>Apply for this Job</h2>
+            <JobApply job={job} />
         </div>
     );
 };
