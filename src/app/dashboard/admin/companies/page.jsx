6911b32a8d3b94f24bb5d59e88433dc6243d211c
ProgrammingHero1 <programming.hero1@gmail.com>
2026-06-08 14:47:25 -0500

    Ensure Can not post a job without admin approval

---
+++ src/app/dashboard/admin/companies/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const AdminCompaniesPage = () => {
+    return (
+        <div>
+            <h2>Companies for review</h2>
+        </div>
+    );
+};
+
+export default AdminCompaniesPage;
