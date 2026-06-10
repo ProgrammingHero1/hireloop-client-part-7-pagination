445d62079e438f300726f780327961d8027cdc52
ProgrammingHero1 <programming.hero1@gmail.com>
2026-06-10 17:48:58 -0500

    handle 401 and 403 status code

--- src/lib/core/server.js
+++ src/lib/core/server.js
@@ -1,3 +1,4 @@
+import { redirect } from "next/navigation";
 import { getUserToken } from "./session";
 
 const baseUrl = process.env.NEXT_PUBLIC_BASE_URL;
@@ -12,20 +13,20 @@ export const authHeader = async () => {
 
 export const serverFetch = async (path) => {
     const res = await fetch(`${baseUrl}${path}`);
-    // handle 401, 404, 403
-    return res.json();
+    
+    return handleStatusCode(res);
 }
 
-export const protectedFetch = async(path) =>{
-    const res = await fetch(`${baseUrl}${path}`, 
+export const protectedFetch = async (path) => {
+    const res = await fetch(`${baseUrl}${path}`,
         {
             headers: await authHeader()
         }
     );
 
     // handle 401, 403
-    
-    return res.json();
+
+    return handleStatusCode(res);
 }
 
 
@@ -39,7 +40,19 @@ export const serverMutation = async (path, data, method = 'POST') => {
         body: JSON.stringify(data),
     });
 
-    // handle 401, 404, 403
 
-    return res.json();
+    return handleStatusCode(res);
+}
+
+
+// handle 401, 404, 403
+const handleStatusCode = res => {
+    if (res.status === 401) {
+        redirect('/unauthorized')
+    }
+    else if (res.status === 403) {
+        redirect('/unauthorized');
+    }
+
+    return res.json()
 }
